fdf119ec51878d43c5a451a958e3faa839f9230f
aardito2 <anthony.ardito@gmail.com>
2017-05-16 20:46:29 -0400

    Update parseCondition.js
    Generalize error message for parseCondition

--- src/plugins/shared/parseCondition.js
+++ src/plugins/shared/parseCondition.js
@@ -4,11 +4,11 @@ export default function parseCondition(condition, t) {
 	const parsed = parse(condition).program.body;
 
 	if (parsed.length !== 1) {
-		throw new Error('Invalid v-if/else-if condition');
+		throw new Error('Invalid condition');
 	}
 
 	if (!t.isExpressionStatement(parsed[0])) {
-		throw new Error('Invalid v-if/else-if condition');
+		throw new Error('Invalid condition');
 	}
 
 	return parsed[0].expression;
